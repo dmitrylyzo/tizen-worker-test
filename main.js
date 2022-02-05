1e9cff8252b90cb5570dbb3081dddd7ae9b42c6d
Dmitry Lyzo <ashephard0@gmail.com>
2022-02-05 23:55:40 +0300

    Add more tests

--- main.js
+++ main.js
@@ -5,3 +5,28 @@ var worker = new Worker('worker.js');
 worker.onerror = function (e) {
     console.error('worker error', e);
 };
+
+var xhr1 = new XMLHttpRequest();
+xhr1.open('GET', 'test.data', true);
+xhr1.onload = function () {
+    console.log('main load 1', xhr1.response);
+};
+xhr1.onerror = function (e) {
+    console.error('main error', e);
+};
+xhr1.send(null);
+
+var xhr2 = new XMLHttpRequest();
+xhr2.open('GET', 'file:///opt/usr/apps/GV24KP5ul8/res/wgt/test.data', true);
+xhr2.onload = function () {
+    console.log('main load 2', xhr2.response);
+};
+xhr2.onerror = function (e) {
+    console.error('main error', e);
+};
+xhr2.send(null);
+
+var xhr3 = new XMLHttpRequest();
+xhr3.open('GET', 'test.data', false);
+xhr3.send(null);
+console.log('main load 3', xhr3.response);
--- worker.js
+++ worker.js
@@ -3,10 +3,10 @@ console.log('Worker');
 var xhr1 = new XMLHttpRequest();
 xhr1.open('GET', 'test.data', true);
 xhr1.onload = function () {
-    console.log('load 1');
+    console.log('worker load 1', xhr1.response);
 };
 xhr1.onerror = function (e) {
-    console.error('error', e);
+    console.error('worker error', e);
 };
 xhr1.send(null);
 
@@ -14,10 +14,10 @@ xhr1.send(null);
 var xhr2 = new XMLHttpRequest();
 xhr2.open('GET', 'file:///opt/usr/apps/GV24KP5ul8/res/wgt/test.data', true);
 xhr2.onload = function () {
-    console.log('load 2');
+    console.log('worker load 2', xhr2.response);
 };
 xhr2.onerror = function (e) {
-    console.error('error', e);
+    console.error('worker error', e);
 };
 xhr2.send(null);
 
@@ -25,4 +25,4 @@ xhr2.send(null);
 var xhr3 = new XMLHttpRequest();
 xhr3.open('GET', 'test.data', false);
 xhr3.send(null);
-console.log('load 3', xhr3.response);
+console.log('worker load 3', xhr3.response);
