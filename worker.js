72f2f0e1dae6f723d51faec8749f3b606e7573b8
Dmitry Lyzo <ashephard0@gmail.com>
2022-02-05 20:27:56 +0300

    Add absolute path test

--- worker.js
+++ worker.js
@@ -1,11 +1,22 @@
 console.log('Worker');
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'test.data', true);
-xhr.onload = function () {
-    console.log('load');
+var xhr1 = new XMLHttpRequest();
+xhr1.open('GET', 'test.data', true);
+xhr1.onload = function () {
+    console.log('load 1');
 };
-xhr.onerror = function (e) {
+xhr1.onerror = function (e) {
     console.error('error', e);
 };
-xhr.send(null);
+xhr1.send(null);
+
+// Tizen 5 doesn't work with relative path for some reason
+var xhr2 = new XMLHttpRequest();
+xhr2.open('GET', 'file:///opt/usr/apps/GV24KP5ul8/res/wgt/test.data', true);
+xhr2.onload = function () {
+    console.log('load 2');
+};
+xhr2.onerror = function (e) {
+    console.error('error', e);
+};
+xhr2.send(null);
