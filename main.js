cbf5a5ef7d7990f69cbddacccca6471138fb2b1c
Dmitry Lyzo <ashephard0@gmail.com>
2022-02-14 23:41:50 +0300

    Rewrite and visualize tests

--- main.js
+++ main.js
@@ -1,32 +1,80 @@
 console.log('Main');
 
-var worker = new Worker('worker.js');
+function getAbsUrl() {
+    var platformVersion = tizen.systeminfo.getCapability('http://tizen.org/feature/platform.version').split('.');
 
-worker.onerror = function (e) {
-    console.error('worker error', e);
-};
+    if (parseInt(platformVersion[0], 10) <= 3) {
+        return 'file:///opt/usr/home/owner/apps_rw/GV24KP5ul8/res/wgt/test.data';
+    }
 
-var xhr1 = new XMLHttpRequest();
-xhr1.open('GET', 'test.data', true);
-xhr1.onload = function () {
-    console.log('main load 1', xhr1.response);
-};
-xhr1.onerror = function (e) {
-    console.error('main error', e);
-};
-xhr1.send(null);
+    return 'file:///opt/usr/apps/GV24KP5ul8/res/wgt/test.data';
+}
 
-var xhr2 = new XMLHttpRequest();
-xhr2.open('GET', 'file:///opt/usr/apps/GV24KP5ul8/res/wgt/test.data', true);
-xhr2.onload = function () {
-    console.log('main load 2', xhr2.response);
-};
-xhr2.onerror = function (e) {
-    console.error('main error', e);
-};
-xhr2.send(null);
+function runAsync(name, url) {
+    var test = document.querySelector('#' + name);
 
-var xhr3 = new XMLHttpRequest();
-xhr3.open('GET', 'test.data', false);
-xhr3.send(null);
-console.log('main load 3', xhr3.response);
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onload = function () {
+        console.log(name, xhr.response);
+        test.classList.add('success');
+    };
+    xhr.onerror = function (e) {
+        console.error(name, e);
+        test.classList.add('failed');
+    };
+    xhr.send(null);
+}
+
+function runSync(name, url) {
+    var test = document.querySelector('#' + name);
+
+    try {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, false);
+        xhr.send(null);
+
+        console.log(name, xhr.response);
+
+        test.classList.add('success');
+    } catch (e) {
+        test.classList.add('failed');
+    }
+}
+
+window.onload = function () {
+    var worker = new Worker('worker.js');
+
+    worker.onerror = function (e) {
+        console.error('worker error', e);
+    };
+
+    worker.onmessage = function (e) {
+        var test = document.querySelector('#' + e.data.name);
+        if (e.data.success) {
+            test.classList.add('success');
+        } else {
+            test.classList.add('failed');
+        }
+    }
+
+    runAsync('main1', 'test.data');
+    runAsync('main2', getAbsUrl());
+    runSync('main3', 'test.data');
+
+    worker.postMessage({
+        name: 'worker1',
+        url: 'test.data'
+    });
+
+    worker.postMessage({
+        name: 'worker2',
+        url: getAbsUrl()
+    });
+
+    worker.postMessage({
+        name: 'worker3',
+        url: 'test.data',
+        sync: true
+    });
+}
--- worker.js
+++ worker.js
@@ -1,28 +1,49 @@
 console.log('Worker');
 
-var xhr1 = new XMLHttpRequest();
-xhr1.open('GET', 'test.data', true);
-xhr1.onload = function () {
-    console.log('worker load 1', xhr1.response);
-};
-xhr1.onerror = function (e) {
-    console.error('worker error', e);
-};
-xhr1.send(null);
-
 // Tizen 5 doesn't work with relative path for some reason
-var xhr2 = new XMLHttpRequest();
-xhr2.open('GET', 'file:///opt/usr/apps/GV24KP5ul8/res/wgt/test.data', true);
-xhr2.onload = function () {
-    console.log('worker load 2', xhr2.response);
-};
-xhr2.onerror = function (e) {
-    console.error('worker error', e);
-};
-xhr2.send(null);
-
 // But sync request works on Tizen 5
-var xhr3 = new XMLHttpRequest();
-xhr3.open('GET', 'test.data', false);
-xhr3.send(null);
-console.log('worker load 3', xhr3.response);
+
+function runAsync(name, url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onload = function () {
+        console.log(name, xhr.response);
+        self.postMessage({
+            name: name,
+            success: true
+        });
+    };
+    xhr.onerror = function (e) {
+        console.error(name, e);
+        self.postMessage({
+            name: name,
+            success: false
+        });
+    };
+    xhr.send(null);
+}
+
+function runSync(name, url) {
+    try {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, false);
+        xhr.send(null);
+
+        console.log(name, xhr.response);
+
+        self.postMessage({
+            name: name,
+            success: true
+        });
+    } catch (e) {
+        self.postMessage({
+            name: name,
+            success: false
+        });
+    }
+}
+
+self.onmessage = function (e) {
+    var msg = e.data;
+    (msg.sync ? runSync : runAsync)(msg.name, msg.url);
+}
